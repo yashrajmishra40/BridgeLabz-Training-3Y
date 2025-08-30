b4c7ff503f29c003f63e5cdd6c85e963a5a70c8f
Yashraj Mishra <145664598+yashrajmishra40@users.noreply.github.com>
2025-08-30 15:50:11 +0530

    [Yashraj] Add all files of Level 2

---
+++ Control-Flow/Level 2/EmployeeBonus.java
@@ -0,0 +1,22 @@
+package ControlFlow.Level2;
+
+import java.util.Scanner;
+
+public class EmployeeBonus {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter salary: ");
+        double salary = sc.nextDouble();
+        System.out.print("Enter years of service: ");
+        int years = sc.nextInt();
+
+        if (years > 5) {
+            double bonus = salary * 0.05;
+            System.out.println("Bonus = " + bonus);
+        } else {
+            System.out.println("No bonus");
+        }
+        sc.close(); 
+    }
+    
+}
