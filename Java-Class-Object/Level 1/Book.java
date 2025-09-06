1c854bafe9f2766d24048e03fa64b87a2fbd39ce
Yashraj Mishra <145664598+yashrajmishra40@users.noreply.github.com>
2025-09-06 16:51:55 +0530

    Added Java-Class-Object

---
+++ Java-Class-Object/Level 1/Book.java
@@ -0,0 +1,23 @@
+package OOPStyle.Level1;
+class Book {
+    String title;
+    String author;
+    double price;
+
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    void displayDetails() {
+        System.out.println("Book Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        Book b = new Book("Java Basics", "James Gosling", 599.50);
+        b.displayDetails();
+    }
+}
